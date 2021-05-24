fa753ef12342fb384b7c60694305239c3728a037
WhaleMountain <whalemountain4@gmail.com>
2021-05-24 15:03:02 +0900

    support graphql

--- filter-unique-requests.py
+++ filter-unique-requests.py
@@ -3,15 +3,20 @@
 from burp import IBurpExtender
 from burp import IHttpListener
 from burp import IProxyListener
+from burp import IParameter
+from java.io import PrintWriter
 
 class BurpExtender(IBurpExtender, IHttpListener, IProxyListener):
     extentionName       = "Filter Unique Requests"
     color               = "cyan" # red, magenta, yellow, green, cyan, blue, pink, purple, black
-    uniqueRequestUrls   = []
+    uniqueRequests      = {} # Key: request url, value: request params
     
     def	registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
+
+        self._stdout = PrintWriter(callbacks.getStdout(), True)
+        #self._stdout.println(param.getValue())
 
         callbacks.setExtensionName(self.extentionName)
         callbacks.registerHttpListener(self)
@@ -23,12 +28,21 @@ class BurpExtender(IBurpExtender, IHttpListener, IProxyListener):
 
         requestInfo = self._helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
         url = requestInfo.getUrl()
+        params = requestInfo.getParameters()
         
         # 対象スコープでない場合は無視
         if not self._callbacks.isInScope(url):
             return
-        
-        # uniqueRequestUrls にないリクエストを送信したら、そのヒストリーの行をハイライトする
-        if url not in self.uniqueRequestUrls:
-            self.uniqueRequestUrls.append(url)
+
+        query = ""
+        for param in params:
+            if param.getType() == IParameter.PARAM_JSON and param.getName() == "query":
+                query = param.getValue()     
+
+        if url in self.uniqueRequests.keys():
+            if query not in self.uniqueRequests[url]:
+                self.uniqueRequests[url].append(query)
+                messageInfo.setHighlight(self.color)
+        else:
+            self.uniqueRequests[url] = [query]
             messageInfo.setHighlight(self.color)
