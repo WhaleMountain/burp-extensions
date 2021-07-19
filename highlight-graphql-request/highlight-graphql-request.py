1766bdd6ba1dcb456e7bdbf13aeb7bb598e05c80
WhaleMountain <whalemountain4@gmail.com>
2021-07-19 16:55:25 +0900

    support proxyHistory

--- highlight-graphql-request/highlight-graphql-request.py
+++ highlight-graphql-request/highlight-graphql-request.py
@@ -3,12 +3,14 @@
 from burp import IBurpExtender
 from burp import IHttpListener
 from burp import IParameter
+from burp import IBurpExtenderCallbacks
 from java.io import PrintWriter
 
 class BurpExtender(IBurpExtender, IHttpListener):
     extentionName       = "Highlight GraphQL Request"
     color               = "cyan" # red, magenta, yellow, green, cyan, blue, pink, purple, black
-    graphqlRequests     = []
+    endpoint            = "/graphql"
+    graphqlRequests     = []   
     
     def	registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
@@ -17,6 +19,11 @@ class BurpExtender(IBurpExtender, IHttpListener):
 
         callbacks.setExtensionName(self.extentionName)
         callbacks.registerHttpListener(self)
+
+        proxyHistReqs = callbacks.getProxyHistory()
+        for proxyHistReq in proxyHistReqs:
+            requestInfo = self._helpers.analyzeRequest(proxyHistReq)
+            self.uniqueRequest(requestInfo)
     
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
         # リクエストのみ
@@ -24,17 +31,22 @@ class BurpExtender(IBurpExtender, IHttpListener):
             return
 
         requestInfo = self._helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
+        if self.uniqueRequest(requestInfo):
+            messageInfo.setHighlight(self.color)
+
+    # ユニークなGraphQLのリクエストならTrueを返す
+    def uniqueRequest(self, requestInfo):
         url     = requestInfo.getUrl()
         method  = requestInfo.getMethod()
         params  = requestInfo.getParameters()
         
         # 対象スコープでない場合は無視
         if not self._callbacks.isInScope(url):
-            return
+            return False
 
         # Only GraphQL
-        if "/graphql" not in url.toString():
-            return
+        if self.endpoint not in url.toString():
+            return False
 
         query = ""
         for param in params:
@@ -43,4 +55,7 @@ class BurpExtender(IBurpExtender, IHttpListener):
 
         if query != "" and query not in self.graphqlRequests:
             self.graphqlRequests.append(query)
-            messageInfo.setHighlight(self.color)
+            return True
+        
+        return False
+
