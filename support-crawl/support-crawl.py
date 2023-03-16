10236d5952ee64442fb87f6d10638c9debe7c65a
mk <33239408+WhaleMountain@users.noreply.github.com>
2023-03-16 14:24:10 +0900

    add parameter check

--- support-crawl/support-crawl.py
+++ support-crawl/support-crawl.py
@@ -8,17 +8,21 @@ from burp import IContextMenuInvocation
 from javax.swing import JMenuItem
 from java.awt import Toolkit
 from java.awt.datatransfer import Clipboard, StringSelection
+import random
+import string
 
 class BurpExtender(IBurpExtender, IProxyListener, IContextMenuFactory, IContextMenuInvocation):
     def __init__(self):
         self.extentionName = "Support Crawl"
-        self.menuName = "Copy Request TSV (Full)"
-        self.comp_color = "gray"
-        self.copy_color = 'pink'
-        self.comment = "Already copied"
+        #self.menuName = "Copy Request TSV (Full)"
+        self.menuName = "Parameter Check"
+        #self.comp_color = "gray"
+        #self.copy_color = 'pink'
+        self.comp_color = "none" # red, magenta, yellow, green, cyan, blue, pink, purple, gray, none=white
+        self.comment = "No.{}, <= #{} "
         self.rdb = RequestDictDB()
         self.rcmp = RequestComparsion()
-        self.cptsv = CopyToTsv()
+        #self.cptsv = CopyToTsv()
 
     def	registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
@@ -26,87 +30,99 @@ class BurpExtender(IBurpExtender, IProxyListener, IContextMenuFactory, IContextM
 
         callbacks.setExtensionName(self.extentionName)
         callbacks.registerContextMenuFactory(self)
-        callbacks.registerProxyListener(self)
+        #callbacks.registerProxyListener(self)
 
-    def processProxyMessage(self, messageIsRequest, message):
-        if not messageIsRequest:
-            return
-
-        messageInfo = message.getMessageInfo()
-        requestInfo = self._helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
-        url             = requestInfo.getUrl()
-        method          = requestInfo.getMethod()
-        headers         = requestInfo.getHeaders()
-        parameters      = requestInfo.getParameters()
-        method_url      = '{}{}://{}{}'.format(method, url.getProtocol(), url.getHost(), url.getPath())
-
-        if not self._callbacks.isInScope(url):
-            return
-        
-        if self.rdb.exists_request(method_url):
-            saved_request = self.rdb.get_request_info(method_url)
-            if (self.rcmp.comparsion_header(headers, saved_request['Headers'])
-            and self.rcmp.comparsion_parameter(parameters, saved_request['Parameters'])
-            and self.rcmp.comparsion_cookies(parameters, saved_request['Cookies'])):
-                messageInfo.setHighlight(self.comp_color)
-                messageInfo.setComment(self.comment)
+    #def processProxyMessage(self, messageIsRequest, message):
+        #if not messageIsRequest:
+        #    return
+        #messageInfo = message.getMessageInfo()
+        #requestInfo = self._helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
+        #url             = requestInfo.getUrl()
+        #method          = requestInfo.getMethod()
+        #headers         = requestInfo.getHeaders()
+        #parameters      = requestInfo.getParameters()
+        #method_url      = '{}{}://{}{}'.format(method, url.getProtocol(), url.getHost(), url.getPath())
+        #if not self._callbacks.isInScope(url):
+        #    return
+        #if self.rdb.exists_request(method_url):
+        #    saved_request = self.rdb.get_request_info(method_url)
+        #    if (self.rcmp.comparsion_header(headers, saved_request['Headers'])
+        #    and self.rcmp.comparsion_parameter(parameters, saved_request['Parameters'])
+        #    and self.rcmp.comparsion_cookies(parameters, saved_request['Cookies'])):
+        #        messageInfo.setHighlight(self.comp_color)
+        #        messageInfo.setComment(self.comment)
 
     def createMenuItems(self, invocation):
         menu = []
-        menu.append(JMenuItem(self.menuName, actionPerformed=lambda x, inv=invocation: self.copyToTsv(inv)))
+        #menu.append(JMenuItem(self.menuName, actionPerformed=lambda x, inv=invocation: self.copyToTsv(inv)))
+        menu.append(JMenuItem(self.menuName, actionPerformed=lambda x, inv=invocation: self.parameterCheck(inv)))
         return menu
-    
-    def copyToTsv(self, inv):
-        tsv = ""
-        for messageInfo in inv.getSelectedMessages():
+
+    def parameterCheck(self, inv):
+        for idx, messageInfo in enumerate(inv.getSelectedMessages()):
             requestInfo = self._helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
-            tsv += self.cptsv.makeTsv(requestInfo)
-            self.rdb.set_request(requestInfo)
-            #messageInfo.setHighlight(self.copy_color)
-        self.cptsv.copyTsv(tsv)
+            url             = requestInfo.getUrl()
+            method          = requestInfo.getMethod()
+            parameters      = requestInfo.getParameters()
+            method_url      = '{}{}://{}{}'.format(method, url.getProtocol(), url.getHost(), url.getPath())
 
-class CopyToTsv():
-    def __init__(self):
-        self.toolkit = Toolkit.getDefaultToolkit()
-        self.clipboard = self.toolkit.getSystemClipboard()
-        self.tsv_head = "\"{method}\"\t\"{uri}\"\t\"-\"\t\"-\"\t\"-\"\n"
-        self.tsv_data = "\"\"\t\"\"\t\"{kind}\"\t\"{key}\"\t\"{value}\"\n"
+            messageInfo.setComment("No.{}".format(idx + 1))
+            for rkey, saved_request in self.rdb.get_request_info(method_url).items():
+                if self.rcmp.comparsion_parameter(parameters, saved_request['Parameters']):
+                    messageInfo.setComment(self.comment.format(idx+1, saved_request['Request number']))
+                    messageInfo.setHighlight(self.comp_color)
+                    break
 
-    def makeTsv(self, requestInfo):
-        url             = requestInfo.getUrl()
-        method          = requestInfo.getMethod()
-        headers         = requestInfo.getHeaders()
-        parameters      = requestInfo.getParameters()
-        turl      = '{}://{}{}'.format(url.getProtocol(), url.getHost(), url.getPath())
+            self.rdb.set_request(idx+1, requestInfo)
+        self.rdb.delete_requests()
+    
+    #def copyToTsv(self, inv):
+        #tsv = ""
+        #for messageInfo in inv.getSelectedMessages():
+        #    requestInfo = self._helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
+        #    tsv += self.cptsv.makeTsv(requestInfo)
+        #    self.rdb.set_request(requestInfo)
+        #    messageInfo.setHighlight(self.copy_color)
+        #self.cptsv.copyTsv(tsv)
 
-        tsv = self.tsv_head.format(method=method, uri=turl)
+#class CopyToTsv():
+    #def __init__(self):
+        #self.toolkit = Toolkit.getDefaultToolkit()
+        #self.clipboard = self.toolkit.getSystemClipboard()
+        #self.tsv_head = "\"{method}\"\t\"{uri}\"\t\"-\"\t\"-\"\t\"-\"\n"
+        #self.tsv_data = "\"\"\t\"\"\t\"{kind}\"\t\"{key}\"\t\"{value}\"\n"
 
-        for idx, path in enumerate(url.getPath().split("/")):
-            if path == "":
-                continue
-            tsv += self.tsv_data.format(kind="Path", key=idx, value=path)
-
-        for header in headers:
-            h = header.split(": ", 1)
-            if len(h) == 2 and h[0] != "Cookie":
-                tsv += self.tsv_data.format(kind="Header", key=h[0], value=h[1].encode('utf-8'))
-
-        for parameter in parameters:
-            if parameter.getType() == IParameter.PARAM_COOKIE:
-                tsv += self.tsv_data.format(kind="Cookie", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
-            elif parameter.getType() == IParameter.PARAM_URL:
-                tsv += self.tsv_data.format(kind="URL", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
-            elif parameter.getType() == IParameter.PARAM_BODY:
-                tsv += self.tsv_data.format(kind="Body", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
-            elif parameter.getType() == IParameter.PARAM_JSON:
-                tsv += self.tsv_data.format(kind="Body", key="jsonData["+parameter.getName()+"]", value=parameter.getValue().encode('utf-8'))
-            else:
-                tsv += self.tsv_data.format(kind="Unknown", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
-        return tsv
+    #def makeTsv(self, requestInfo):
+        #url             = requestInfo.getUrl()
+        #method          = requestInfo.getMethod()
+        #headers         = requestInfo.getHeaders()
+        #parameters      = requestInfo.getParameters()
+        #turl      = '{}://{}{}'.format(url.getProtocol(), url.getHost(), url.getPath())
+        #tsv = self.tsv_head.format(method=method, uri=turl)
+        #for idx, path in enumerate(url.getPath().split("/")):
+        #    if path == "":
+        #        continue
+        #    tsv += self.tsv_data.format(kind="Path", key=idx, value=path)
+        #for header in headers:
+        #    h = header.split(": ", 1)
+        #    if len(h) == 2 and h[0] != "Cookie":
+        #        tsv += self.tsv_data.format(kind="Header", key=h[0], value=h[1].encode('utf-8'))
+        #for parameter in parameters:
+        #    if parameter.getType() == IParameter.PARAM_COOKIE:
+        #        tsv += self.tsv_data.format(kind="Cookie", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
+        #    elif parameter.getType() == IParameter.PARAM_URL:
+        #        tsv += self.tsv_data.format(kind="URL", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
+        #    elif parameter.getType() == IParameter.PARAM_BODY:
+        #        tsv += self.tsv_data.format(kind="Body", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
+        #    elif parameter.getType() == IParameter.PARAM_JSON:
+        #        tsv += self.tsv_data.format(kind="Body", key="jsonData["+parameter.getName()+"]", value=parameter.getValue().encode('utf-8'))
+        #    else:
+        #        tsv += self.tsv_data.format(kind="Unknown", key=parameter.getName(), value=parameter.getValue().encode('utf-8'))
+        #return tsv
         
-    def copyTsv(self, tsv):
-        selection = StringSelection(tsv)
-        self.clipboard.setContents(selection, selection)
+    #def copyTsv(self, tsv):
+        #selection = StringSelection(tsv)
+        #self.clipboard.setContents(selection, selection)
 
 class RequestComparsion():
     # ヘッダーの比較
@@ -138,39 +154,44 @@ class RequestDictDB():
     def __init__(self):
         self.__REQUEST_DATA = {}
 
-    def set_request(self, requestInfo):
+    def set_request(self, idx, requestInfo):
         url             = requestInfo.getUrl()
         method          = requestInfo.getMethod()
         headers         = requestInfo.getHeaders()
         parameters      = requestInfo.getParameters()
-        method_url      = '{}{}://{}{}'.format(method, url.getProtocol(), url.getHost(), url.getPath())
+        randomstr       = "".join([random.choice(string.ascii_lowercase + string.digits) for i in range(5)])
+        method_url      = '{}{}://{}{}#{}'.format(method, url.getProtocol(), url.getHost(), url.getPath(), randomstr)
 
-        self.__REQUEST_DATA[method_url] = {"Headers": {}, "Cookies": {}, "Parameters": {}}
-        self.set_headers(method_url, headers)
-        self.set_parameters(method_url, parameters)
-        self.set_cookies(method_url, parameters)
+        self.__REQUEST_DATA[method_url] = {"Request number": 0, "Headers": {}, "Cookies": {}, "Parameters": {}}
+        self.__REQUEST_DATA[method_url]['Request number'] = idx
+        self.__set_headers(method_url, headers)
+        self.__set_parameters(method_url, parameters)
+        self.__set_cookies(method_url, parameters)
 
-    def set_headers(self, key, headers):
+    def __set_headers(self, key, headers):
         data = self.__REQUEST_DATA[key]['Headers']
         for header in headers:
             h = header.split(": ", 1)
             if len(h) == 2 and h[0] != "Cookie":
                 data[h[0]] = h[1]
 
-    def set_parameters(self, key, parameters):
+    def __set_parameters(self, key, parameters):
         data = self.__REQUEST_DATA[key]['Parameters']
         for parameter in parameters:
             if parameter.getType() != IParameter.PARAM_COOKIE:
                 data[parameter.getName()] = parameter.getValue()
 
-    def set_cookies(self, key, cookies):
+    def __set_cookies(self, key, cookies):
         data = self.__REQUEST_DATA[key]['Cookies']
         for cookie in cookies:
             if cookie.getType() == IParameter.PARAM_COOKIE:
                 data[cookie.getName()] = cookie.getValue()
 
     def get_request_info(self, key):
-        return self.__REQUEST_DATA[key]
+        return dict(filter(lambda key_value: key+"#" in key_value[0], self.__REQUEST_DATA.items()))
 
     def exists_request(self, key):
         return key in self.__REQUEST_DATA.keys()
+    
+    def delete_requests(self):
+        self.__REQUEST_DATA.clear()
